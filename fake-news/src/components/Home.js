397fd3a761139bc304e06d2c72a85a6c87ccc766
Shaun Latip <shaunlatip@gmail.com>
2020-01-25 18:04:40 -0500

    Updated state structure for comments

--- fake-news/src/components/Home.js
+++ fake-news/src/components/Home.js
@@ -29,7 +29,8 @@ class Home extends Component {
         return (
             <body>
                 <div>
-                    <hl>Fake News App!</hl>
+                    <hl>We are the first crowdsourced platform on the internet harnessing the power of the public to verify the truthfulness forwarded messages and media.
+If you received a photo, message or link that you'd like to verify, select the appropriate type from the dropdown box below and upload your ticket to continue.</hl>
                 </div>
                 <select value={this.state.searchType} onChange={this.changeHandler}>
                     <option value='link'>link</option>
