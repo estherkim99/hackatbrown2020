c9461354a49bbb512e2a846d7f777427fee88660
Shaun Latip <shaunlatip@gmail.com>
2020-01-25 15:25:30 -0500

    Added additional class component

---
+++ fake-news/src/components/Tickets.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render() {
+        return (
+            <div className="App">
+                <hl>Tickets</hl>
+            </div>
+        );
+    }
+}
+
+function App() {
+
+  state = {
+    
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Fake News App, yo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
